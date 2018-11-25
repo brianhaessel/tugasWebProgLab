19436afc58f33babd1201dace64c5f655efd3d47
Greffin28 <greffinroid@ymail.com>
2018-11-25 21:52:36 +0700

    Fixed wrong relationship

--- app/Post.php
+++ app/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
     }
 
     public function user() {
-        return $this->hasOne('App\User');
+        return $this->belongsTo('App\User');
     }
 
     public function categories() {
