6b922d9c00c0d74554efdbade07d239d6637d621
Greffin28 <greffinroid@ymail.com>
2018-11-27 21:57:15 +0700

    Simplified and fixed indentation of home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,64 +7,23 @@
 
 
 @section('content')
-@guest
-<div class="container">
-    <div class="row">
-
-        <div class="contentSeg" id="postSection" >
-
-         @foreach($posts as $post)
-         <div class="post" >
-            <a href="{{ 'storage/'.$post->image }}" target="_blank">
-                <img src="{{ 'storage/'.$post->image }}" width="256" height="256" />
-            </a>
-            <p>{{ $post->title }}</p>
-            <p>{{ $post->user->name }}</p>
-        </div>
-        @endforeach
-    </div>
-</div>
-</div>
-{{ $posts->links() }}
-@else
-
-
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                <div class="panel-body">
-                                @if (session('status'))
-                                    <div class="alert alert-success">
-                                        {{ session('status') }}
-                                    </div>
-                                    @endif
-
-                                    <p>You are logged in!</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-
-                    <div class="container">
-                        <div class="row">
-                            <a href="{{ url('/home') }}">Filter by My Followed Categories</a>
-                            <div class="contentSeg" id="postSection" >
-
-                             @foreach($posts as $post)
-                             <div class="post" >
-                                <a href="{{ 'storage/'.$post->image }}" target="_blank">
-                                    <img src="{{ 'storage/'.$post->image }}" width="256" height="256" />
-                                </a>
-                                <p>{{ $post->title }}</p>
-                                <p>{{ $post->user->name }}</p>
-                            </div>
-                            @endforeach
-                        </div>
+    <div class="container">
+        <div class="row">
+            @auth
+                <a href="{{ url('/home') }}">Filter by My Followed Categories</a>
+            @endauth
+            <div class="contentSeg" id="postSection" >
+                @foreach($posts as $post)
+                    <div class="post" >
+                        <a href="{{ 'storage/'.$post->image }}" target="_blank">
+                            <img src="{{ 'storage/'.$post->image }}" width="256" height="256" />
+                        </a>
+                        <p>{{ $post->title }}</p>
+                        <p>{{ $post->user->name }}</p>
                     </div>
-                </div>
-                {{ $posts->links() }}
-                @endguest
-                @endsection
+                @endforeach
+            </div>
+        </div>
+    </div>
+    {{ $posts->links() }}
+@endsection
