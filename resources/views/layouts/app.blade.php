d6ed7a4a6e5ca8bac2461b47739119d99bea65fb
Greffin28 <greffinroid@ymail.com>
2018-11-27 22:09:24 +0700

    Fixed link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                         Pintester
                         <!-- {{ config('app.name', 'Laravel') }} -->
                     </a>
-                    <form action="" method="GET">
+                    <form action="/home" method="GET">
                         <input id="search" type="text" class="form-control" name="search" value="{{ old('search') }}" placeholder="Search a post" required autofocus>
                         <button type="submit">Search</button>
                     </form>
