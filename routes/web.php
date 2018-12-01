644c65705f6bcb3385ca699a7417fc7ef5d11e30
Greffin28 <greffinroid@ymail.com>
2018-12-01 20:30:46 +0700

    Added middleware for all pages

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,35 @@
 |
 */
 
-
+// Accessible by anyone
 Auth::routes();
 Route::get('/', function() {
 	return redirect('home');
 });
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home/search', 'HomeController@search')->name('search');
-Route::get('/followed_categories', 'HomeController@followedCategories')->name('followed_categories');
-Route::get('/cart', 'TransactionController@index')->name('cart');
-Route::get('/myposts', 'HomeController@myPosts')->name('myposts');
-Route::get('/add', 'PostController@add');
-Route::post('/insert', 'PostController@store')->name('insert');
 Route::get('/post/{id}', 'PostController@view');
-Route::post('/addComment', 'PostController@addComment');
-Route::get('/profile', 'UserController@profile');
-Route::get('/followedCategories', 'UserController@followedCategories');
-Route::post('/deletePost', 'PostController@delete')->name('delete');
-Route::get('/transactionHistory','UserController@transactionHistory')->name('transaction_history');
-Route::get('/manageUser','UserController@manageUser')->name('manage_user');
-Route::get('/manageCategory', 'CategoryController@index')->name('manage_category');
-Route::get('/allTransaction', 'TransactionController@allTransaction')->name('all_transaction');
-Route::get('/addCategory','CategoryController@addCategory')->name('add_category');
-Route::post('/insertCategory', 'CategoryController@store')->name('insert_category');
+
+// Accessible by User/Admin
+Route::middleware(['auth'])->group(function() {
+	Route::get('/followed_categories', 'HomeController@followedCategories')->name('followed_categories');
+	Route::get('/cart', 'TransactionController@index')->name('cart');
+	Route::get('/myposts', 'HomeController@myPosts')->name('myposts');
+	Route::get('/add', 'PostController@add');
+	Route::post('/insert', 'PostController@store')->name('insert');
+	Route::post('/addComment', 'PostController@addComment');
+	Route::get('/profile', 'UserController@profile');
+	Route::get('/followedCategories', 'UserController@followedCategories');
+	Route::post('/deletePost', 'PostController@delete')->name('delete');
+	Route::get('/transactionHistory','UserController@transactionHistory')->name('transaction_history');
+	
+});
+
+// Accessible by Admin
+Route::middleware(['admin'])->group(function() {
+	Route::get('/manageUser','UserController@manageUser')->name('manage_user');
+	Route::get('/manageCategory', 'CategoryController@index')->name('manage_category');
+	Route::get('/allTransaction', 'TransactionController@allTransaction')->name('all_transaction');
+	Route::get('/addCategory','CategoryController@addCategory')->name('add_category');
+	Route::post('/insertCategory', 'CategoryController@store')->name('insert_category');
+});
