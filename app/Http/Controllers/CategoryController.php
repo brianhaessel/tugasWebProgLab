d78e89e294323f3ef7bd7fc9d689439a151f461b
Greffin28 <greffinroid@ymail.com>
2018-12-03 22:45:29 +0700

    Fixed redirect

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -58,7 +58,7 @@ class CategoryController extends Controller
             $category->name = $request->name;
             $category->save();
         
-        return back();
+        return redirect()->route('manage_category');
     }
 
     /**
